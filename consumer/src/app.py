4a7447d2b3a815645cb929694996fe4f8b426bca
alahirajeffrey <alahirajeffrey@gmail.com>
2023-08-06 15:08:48 +0100

    code refactor

--- consumer/src/app.py
+++ consumer/src/app.py
@@ -9,27 +9,35 @@ app = Flask(__name__)
 
 KAFKA_BROKER = os.getenv("KAFKA_BROKER")
 
-if __name__ == '__main__':
-
-    consumer = Consumer({
+consumer = Consumer({
         'bootstrap.servers': KAFKA_BROKER,
         'group.id': 'mygroup',
         'auto.offset.reset': 'earliest'
     })
 
-    consumer.subscribe(['mytopic'])
+def consume_message(topic):
+    '''subscribes to a topic and consume messages'''
+    
+    consumer.subscribe([topic])
 
-    while True:
-        msg = consumer.poll(1.0)
+    try:
+        while True:
+            msg = consumer.poll(1.0)
 
-        if msg is None:
-            continue
-        if msg.error():
-            print("Consumer error: {}".format(msg.error()))
-            continue
+            if msg is None:
+                continue
+            if msg.error():
+                print("Consumer error: {}".format(msg.error()))
+                continue
 
-        print('Received message: {}'.format(msg.value().decode('utf-8')))
+            print('Received message: {}'.format(msg.value().decode('utf-8')))
 
+    except Exception as error:
+        print(error)
+    finally:
         consumer.close()
+
+if __name__ == '__main__':
+    consume_message('tutorial-topic')
 
     app.run(port=5000)
